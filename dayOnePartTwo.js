8f2ffd76d51de45870a75b4e8b92206744cdcdfe
unikorm <adaled00@gmail.com>
2023-12-03 02:09:41 +0100

    second part of first day in progress, but i think there will be again some small thing what i did not realize now

---
+++ dayOnePartTwo.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var regex = /(\d+|(?:one|two|three|four|five|six|seven|eight|nine)\b)/g;
+var wordToNumber = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+};
+var extractNumbers = function (str) {
+    var _a;
+    var intigers = (_a = str.match(regex)) === null || _a === void 0 ? void 0 : _a.map(function (num) {
+        if (isNaN(Number(num))) {
+            return wordToNumber[num];
+        }
+        else {
+            return num;
+        }
+    });
+    return parseInt(intigers ? intigers.join('') : '');
+};
+var filePath = 'input.txt';
+var ValuesArr = [];
+function processFile(filePath) {
+    var fileContent = fs.readFileSync(filePath, 'utf-8');
+    var lines = fileContent.split('\n');
+    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+        var line = lines_1[_i];
+        var numbers = extractNumbers(line);
+        ValuesArr.push(numbers);
+    }
+}
+processFile(filePath);
+var againSt = ValuesArr.map(function (num) { return num.toString(); });
+for (var j = 0; j < againSt.length; j++) {
+    if (againSt[j].length === 1) {
+        againSt[j] += againSt[j];
+    }
+    if (againSt[j].length > 2) {
+        var charFirst = againSt[j][0];
+        var lastChar = againSt[j][againSt[j].length - 1];
+        againSt[j] = ''.concat(charFirst, lastChar);
+    }
+}
+var finalInt = againSt.map(function (num) { return parseInt(num); });
+// const test: number[] = [11, 22, 33, 44];
+var sum = 0;
+for (var k = 0; k < finalInt.length; k++) {
+    sum = sum + finalInt[k];
+}
+console.log(ValuesArr, againSt, finalInt, sum);
---
+++ dayOnePartTwo.ts
@@ -0,0 +1,78 @@
+
+
+import * as fs from 'fs';
+
+
+
+const regex = /(\d+|(?:one|two|three|four|five|six|seven|eight|nine)\b)/g;
+
+const wordToNumber = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
+
+const extractNumbers = (str: string): number => {  // i need return all the numbers in this function (both the digits and letter numbers)
+    const intigers = str.match(regex)?.map((num: string) => {
+        if (isNaN(Number(num))) {
+            return wordToNumber[num];
+        } else {
+            return num;
+        }
+    })
+    return parseInt(intigers ? intigers.join('') : '');
+}
+
+
+
+
+
+
+
+
+
+
+const filePath = 'input.txt';
+const ValuesArr: number[] = []
+function processFile(filePath: string) {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
+
+    for (const line of lines) {
+        const numbers: number = extractNumbers(line);
+        ValuesArr.push(numbers);
+    }
+}
+
+
+processFile(filePath);
+
+const againSt: string[] = ValuesArr.map(num => num.toString());
+for (let j = 0; j < againSt.length; j++) {
+
+    if (againSt[j].length === 1) {
+        againSt[j] += againSt[j];
+    }
+
+    if (againSt[j].length > 2) {
+        const charFirst: string = againSt[j][0];
+        const lastChar: string = againSt[j][againSt[j].length - 1];
+        againSt[j] = ''.concat(charFirst, lastChar);
+    }
+}
+
+const finalInt: number[] = againSt.map(num => parseInt(num));
+// const test: number[] = [11, 22, 33, 44];
+let sum: number = 0;
+for (let k = 0; k < finalInt.length; k++) {
+    sum = sum + finalInt[k];
+}
+
+
+console.log(ValuesArr, againSt, finalInt, sum);
