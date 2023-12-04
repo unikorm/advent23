3c55791be999fdfe5d989bb1fb8d58c9a7f07410
unikorm <adaled00@gmail.com>
2023-12-04 23:04:06 +0100

    yess, day 2 part 1 DONE very soon, too easy to compare it with day 1

--- dTwo.js
+++ dTwo.js
@@ -6,8 +6,11 @@ var regexRed = /\b(?:13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29)\sred\b/
 var regexGreen = /\b(?:14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29)\sgreen\b/;
 var regexBlue = /\b(?:15|16|17|18|19|20|21|22|23|24|25|26|27|28|29)\sblue\b/;
 var regexId = /Game (\d+):/;
+var regexNum = /\d+/;
 var ValuesArr = [];
-function processFile(filePath) {
+var integers = [];
+var final = 0;
+var processFile = function (filePath) {
     var fileContent = fs.readFileSync(filePath, 'utf-8');
     var lines = fileContent.split('\n');
     for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
@@ -16,9 +19,20 @@ function processFile(filePath) {
             console.log("too big");
         else {
             var match = line.match(regexId);
-            ValuesArr.push(match[0]);
+            ValuesArr.push(match[1]);
         }
+        ;
     }
-}
+    ;
+    for (var j = 0; j < ValuesArr.length; j++) {
+        integers.push(parseInt(ValuesArr[j]));
+    }
+    // integers = ValuesArr.map(str => parseInt(str, 10))
+    // integers.forEach(num => final += num);
+    for (var i = 0; i < integers.length; i++) {
+        final += integers[i];
+    }
+    ;
+};
 processFile(filePath);
-console.log(ValuesArr);
+console.log(integers, final);
--- dTwo.ts
+++ dTwo.ts
@@ -7,21 +7,34 @@ const regexRed: RegExp = /\b(?:13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|2
 const regexGreen: RegExp = /\b(?:14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29)\sgreen\b/;
 const regexBlue: RegExp = /\b(?:15|16|17|18|19|20|21|22|23|24|25|26|27|28|29)\sblue\b/;
 const regexId: RegExp = /Game (\d+):/;
+const regexNum: RegExp = /\d+/;
 
 const ValuesArr: string[] = [];
+let integers: number[] = [];
+let final: number = 0;
 
-function processFile(filePath: string) {
+const processFile = (filePath: string): void => {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const lines = fileContent.split('\n');
 
     for (const line of lines) {
         if (regexRed.test(line) || regexGreen.test(line) || regexBlue.test(line)) console.log("too big")
         else {
-            const match = line.match(regexId)
-            ValuesArr.push(match[0]);
-        }
+            const match = line.match(regexId)!
+            ValuesArr.push(match[1]);
+        };
+    };
+
+    for (let j = 0; j < ValuesArr.length; j++) {
+        integers.push(parseInt(ValuesArr[j]))
     }
-}
+    // integers = ValuesArr.map(str => parseInt(str, 10))
+
+    integers.forEach(num => final += num);
+    // for (let i: number = 0; i < integers.length; i++) {
+    //     final += integers[i]; 
+    // };
+};
 
 processFile(filePath);
-console.log(ValuesArr);
+console.log(integers, final);
