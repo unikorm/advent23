7e5e29535d665479f5a87215faf230e366d98466
unikorm <adaled00@gmail.com>
2023-12-04 23:55:22 +0100

    now i try part 2 od second day, i think the hardest part is actual the first, retrieve from each line highest number from each color of cube

---
+++ twoTwo.ts
@@ -0,0 +1,11 @@
+
+import * as fs from 'fs';
+
+const filePath = 'inputDayTwo.txt';
+
+// must retrive max number of each color cube on each line
+// multiplaied max numbers of red, green and blue on each line
+// sum of all of multiplication
+
+// example:  Game 12: 9 green, 2 blue; 4 green, 1 blue, 7 red; 2 green, 1 blue, 5 red -> 9 green, 2 blue, 7red
+
