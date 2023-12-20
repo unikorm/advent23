1bf0d0bf00b1a10931c3724460103f88a7c4c99e
unikorm <adaled00@gmail.com>
2023-12-20 18:26:27 +0100

    for now easy, part two of day two done, i know it is possible make it on less lines of code but idk how so i making it with my actual knowledge

--- 2022/dayTwo/code.ts
+++ 2022/dayTwo/code.ts
@@ -56,4 +56,64 @@ const processFile = (file: string): void => {
     console.log(totalScore);
 }
 
-processFile(filePath);
+// processFile(filePath);
+
+
+// part Two
+
+const processFilePartTwo = (file: string): void => {
+    const fileContent = fs.readFileSync(file, 'utf-8');
+    const lines = fileContent.split('\n');
+
+    const rockOpponent: string = 'A';
+    const paperOpponent: string = 'B';
+    const scissorsOpponent: string = 'C';
+
+    const loseYou: string = 'X';
+    const drawYou: string = 'Y';
+    const winYou: string = 'Z';
+    // 1 -> rock
+    // 2 -> paper
+    // 3 -> scissors
+    // + outcome => 0 for loss;  3 for draw;  6 for win
+
+    let totalScore: number = 0;
+
+    for (const line of lines) {
+        switch (line) {
+            case 'A X':
+                totalScore += 3;
+                break;
+            case 'A Y':
+                totalScore += 4;
+                break;
+            case 'A Z':
+                totalScore += 8;
+                break;
+
+            case 'B X':
+                totalScore += 1;
+                break;
+            case 'B Y':
+                totalScore += 5;
+                break;
+            case 'B Z':
+                totalScore += 9;
+                break;
+
+            case 'C X':
+                totalScore += 2;
+                break;
+            case 'C Y':
+                totalScore += 6;
+                break;
+            case 'C Z':
+                totalScore += 7;
+                break;
+        }
+    }
+
+    console.log(totalScore);
+};
+
+processFilePartTwo(filePath);
