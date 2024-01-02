800bec7b6b9b8a788f32c1e4338fce6cc7b8c06a
unikorm <adaled00@gmail.com>
2024-01-02 19:17:21 +0100

    okey, day six first part done, i realizing using Set is very helpfull in such a situations..

--- 2022/daySix/code.ts
+++ 2022/daySix/code.ts
@@ -1,14 +1,32 @@
 
 import * as fs from 'fs';
 
-const filePath: string = '2022/daySix/test.txt';
+const filePath: string = '2022/daySix/input.txt';
+
+const hasUniqueChar = (value: string): boolean => {
+    const set = new Set(value);
+    return set.size === value.length;
+};
 
 const processFile = (file: string): void => {
     const fileContent = fs.readFileSync(file, 'utf-8');
-    const line = fileContent.split('\n');
-    console.log(line);
-    
+    const line = fileContent.split('');
+    let value: string = '';
+    let point: number = 0;
 
+    for (let char of line) {
+        value += char;
+        point += 1;
+        if (value.length === 4) {
+            let marker = value;
+            if (hasUniqueChar(marker)) {
+                console.log(marker, point);
+                return;
+            };
+
+        value = value.slice(1);
+        };
+    };
 };
 
 processFile(filePath);
